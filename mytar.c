e162cb95622d11d762080acf6bc79fe1b7f1678d
Vlad Covali <covali.vl@gmail.com>
2023-05-14 21:43:51 +0200

    Add phase 2 implementation

--- mytar.c
+++ mytar.c
@@ -8,6 +8,13 @@
 
 #define	BLOCK_SIZE	512
 
+#define	REGTYPE '0'
+#define	AREGTYPE '\0'
+#define VERBOSE "-v"
+#define LIST "-t"
+#define EXTRACT "-x"
+#define FILENAME "-f"
+
 typedef struct posix_header {
 	char name[100];
 	char mode[8];
@@ -28,18 +35,24 @@ typedef struct posix_header {
 	char pad[12];
 } tar_header;
 
-#define	REGTYPE '0'
-#define	AREGTYPE '\0'
-#define	LNKTYPE  '1'
-#define	SYMTYPE  '2'
-#define	CHRTYPE  '3'
-#define	BLKTYPE  '4'
-#define	DIRTYPE  '5'
-#define	FIFOTYPE '6'
-#define	CONTTYPE '7'
+typedef enum operation {
+	OP_NONE,
+	OP_LIST,
+	OP_EXTRACT
+} operation;
 
+bool isTarArchive(FILE *archive) {
+    tar_header header;
+    char block[BLOCK_SIZE];
 
-bool is_zero_block(const char *block) {
+    if (fread(block, BLOCK_SIZE, 1, archive) != 1) {
+        return false;
+    }
+    memcpy(&header, block, BLOCK_SIZE);
+    return strncmp(header.magic, "ustar", 5) == 0;
+}
+
+bool isZeroBlock(const char *block) {
 	for (int i = 0; i < BLOCK_SIZE; ++i) {
 		if (*(block + i) != 0) {
 			return (false);
@@ -48,7 +61,7 @@ bool is_zero_block(const char *block) {
 	return (true);
 }
 
-void exit_with_error(const char *format, ...) {
+void exitError(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
 	vwarnx(format, args);
@@ -56,65 +69,75 @@ void exit_with_error(const char *format, ...) {
 	exit(2);
 }
 
-void parse_args(int argc,
+
+void parseArgs(int argc,
 				char **argv,
-				bool *files_listed,
-				char **archive_filename,
-				int *filenames_start) {
+				bool *verbose,
+				operation *op,
+				char **archiveFilename,
+				int *filenamesStart) {
 
 	int i = 1;
 	while (i < argc) {
-		if (strcmp(*(argv + i), "-t") == 0) {
-			*files_listed = true;
-		} else if (strcmp(*(argv + i), "-f") == 0) {
+		if (strcmp(*(argv + i), LIST) == 0) {
+			*op = OP_LIST;
+			++i;
+		} else if (strcmp(*(argv + i), EXTRACT) == 0) {
+			*op = OP_EXTRACT;
+			++i;
+        }
+        else if (strcmp(*(argv + i), VERBOSE) == 0) {
+            *verbose = true;
+            ++i;
+		} else if (strcmp(*(argv + i), FILENAME) == 0) {
 			if (i + 1 < argc) {
-				*archive_filename = *(argv + i + 1);
-				++i;
-			} else {
-				exit_with_error("Option -f requires an argument");
-			}
+				*archiveFilename = *(argv + i + 1);
+                i += 2;
+            } else {
+                exitError("Option -f requires an argument");
+            }
 		} else {
 			if (i == 1) {
-				exit_with_error("Unknown option: %s", *(argv + i));
-			} else {
-				*filenames_start = i;
-				break;
-			}
+                exitError("Unknown option: %s", *(argv + i));
+            }
+            else {
+                *filenamesStart = i;
+                break;
+            }
 		}
-		++i;
 	}
 }
 
-void list_files(FILE *archive,
+void listFiles(FILE *archive,
 				int argc,
 				char **argv,
-				int filenames_start,
-				char **found_files) {
+				int filenamesStart,
+				char **foundFiles) {
 	tar_header header;
 	char block[BLOCK_SIZE];
-	int continuous_zero_blocks = 0;
-	bool not_found = false;
+	int contZeroBlocks = 0;
+	bool fileNFound = false;
 
 	while (fread(block, BLOCK_SIZE, 1, archive) == 1) {
-		if (is_zero_block(block)) {
-			++continuous_zero_blocks;
-			if (continuous_zero_blocks == 2) {
+		if (isZeroBlock(block)) {
+			++contZeroBlocks;
+			if (contZeroBlocks == 2) {
 				break;
 			}
 			continue;
 		}
 
-		continuous_zero_blocks = 0;
+		contZeroBlocks = 0;
 		memcpy(&header, block, BLOCK_SIZE);
 
 		if (header.typeflag != REGTYPE && header.typeflag != AREGTYPE) {
-			exit_with_error("Unsupported header type: %d", header.typeflag);
+			exitError("Unsupported header type: %d", header.typeflag);
 		}
 
-		if (filenames_start < argc) {
-			for (int i = filenames_start; i < argc; ++i) {
+		if (filenamesStart < argc) {
+			for (int i = filenamesStart; i < argc; ++i) {
 				if (strcmp(*(argv + i), header.name) == 0) {
-					*(found_files + i) = header.name;
+					*(foundFiles + i) = header.name;
 					fprintf(stdout, "%s\n", header.name);
 					fflush(stdout);
 				}
@@ -124,61 +147,168 @@ void list_files(FILE *archive,
 			fflush(stdout);
 		}
 
-		int file_size = strtol(header.size, NULL, 8);
-		int blocks = file_size / BLOCK_SIZE;
+		int fileSize = strtol(header.size, NULL, 8);
+		int blocks = fileSize / BLOCK_SIZE;
 
-		if (file_size % BLOCK_SIZE != 0) {
+		if (fileSize % BLOCK_SIZE != 0) {
 			++blocks;
 		}
 
 		for (int i = 0; i < blocks; ++i) {
 			if (fread(block, BLOCK_SIZE, 1, archive) != 1) {
-				exit_with_error("Unexpected EOF in archive\nmytar: Error is not recoverable: exiting now");
+				exitError("Unexpected EOF in archive\nmytar: Error is not recoverable: exiting now");
 			}
 		}
 	}
 
-	if (continuous_zero_blocks == 1) {
+	if (contZeroBlocks == 1) {
 		warnx("A lone zero block at %ld", ftell(archive) / BLOCK_SIZE);
 	}
 
-	if (filenames_start < argc) {
-		for (int i = filenames_start; i < argc; ++i) {
-			if (*(found_files + i) == NULL) {
+	if (filenamesStart < argc) {
+		for (int i = filenamesStart; i < argc; ++i) {
+			if (*(foundFiles + i) == NULL) {
 				warnx("%s: Not found in archive", *(argv + i));
-				not_found = true;
+				fileNFound = true;
 			}
 		}
 	}
 
-	if (not_found) {
-		exit_with_error("Exiting with failure status due to previous errors");
+	if (fileNFound) {
+		exitError("Exiting with failure status due to previous errors");
 	}
+}
+
+void extractFiles(FILE *archive, 
+					int argc,
+					char **argv,
+					int filenamesStart,
+					char **foundFiles,
+					bool verbose)
+{
+	tar_header header;
+	char block[BLOCK_SIZE];
+	int contZeroBlocks = 0;
+    bool fileNFound = false;
+
+	while (fread(block, BLOCK_SIZE, 1, archive) == 1) {
+		if (isZeroBlock(block)) {
+			++contZeroBlocks;
+			if (contZeroBlocks == 2) {
+				break;
+			}
+			continue;
+		}
+
+		contZeroBlocks = 0;
+		memcpy(&header, block, BLOCK_SIZE);
+
+		if (header.typeflag != REGTYPE && header.typeflag != AREGTYPE) {
+			exitError("Unsupported header type: %d", header.typeflag);
+		}
+
+		int fileSize = strtol(header.size, NULL, 8);
+		int blocks = fileSize / BLOCK_SIZE;
+
+		if (fileSize % BLOCK_SIZE != 0) {
+			++blocks;
+		}
+
+		bool extractFile = true;
+		if (filenamesStart < argc) {
+			extractFile = false;
+			for (int i = filenamesStart; i < argc; ++i) {
+				if (strcmp(*(argv + i), header.name) == 0) {
+					*(foundFiles + i) = header.name;
+					extractFile = true;
+					break;
+				}
+			}
+		}
+
+		if (extractFile) {
+			FILE *outputFile = fopen(header.name, "wb");
+			if (outputFile == NULL) {
+				exitError("Cannot create file %s", header.name);
+			}
+
+			if (verbose) {
+				fprintf(stdout, "%s\n", header.name);
+				fflush(stdout);
+			}
+
+			for (int i = 0; i < blocks; ++i) {
+				if (fread(block, BLOCK_SIZE, 1, archive) != 1) {
+					fclose(outputFile);
+					exitError("Unexpected EOF in archive\nmytar: Error is not recoverable: exiting now");
+				}
+
+				int bytesToWrite = (i == blocks - 1 && fileSize % BLOCK_SIZE != 0) ? fileSize % BLOCK_SIZE : BLOCK_SIZE;
+				fwrite(block, bytesToWrite, 1, outputFile);
+			}
+
+			fclose(outputFile);
+		} else {
+			for (int i = 0; i < blocks; ++i) {
+				if (fread(block, BLOCK_SIZE, 1, archive) != 1) {
+					exitError("Unexpected EOF in archive\nmytar: Error is not recoverable: exiting now");
+				}
+			}
+		}
+	}
+
+    if (contZeroBlocks == 1) {
+		warnx("A lone zero block at %ld", ftell(archive) / BLOCK_SIZE);
+	}
+
+    if (filenamesStart < argc) {
+        for (int i = filenamesStart; i < argc; ++i) {
+            if (*(foundFiles + i) == NULL) {
+                warnx("%s: Not found in archive", *(argv + i));
+                fileNFound = true;
+            }
+        }
+    }
+
+    if (fileNFound) {
+        exitError("Exiting with failure status due to previous errors");
+    }
 }
 
 int main(int argc, char **argv) {
 	if (argc < 3) {
-		exit_with_error("Usage: %s -f -t [archive-filename] [file1] [file2] ...", argv[0]);
+		exitError("Usage: %s -f -t [archive-filename] [file1] [file2] ...", argv[0]);
 	}
 
-	bool files_listed = false;
-	char *archive_filename = NULL;
-	int filenames_start = argc;
-	char *found_files[argc];
-	memset(found_files, 0, sizeof (found_files));
-	parse_args(argc, argv, &files_listed, &archive_filename, &filenames_start);
+	operation op = OP_NONE;
+	bool verbose = false;
+	char *archiveFilename = NULL;
+	int filenamesStart = argc;
+	char *foundFiles[argc];
 
-	if (!files_listed || archive_filename == NULL) {
-		exit_with_error("Usage: %s -f -t [archive-filename] [file1] [file2] ...", argv[0]);
+	memset(foundFiles, 0, sizeof (foundFiles));
+	parseArgs(argc, argv, &verbose, &op, &archiveFilename, &filenamesStart);
+	if (op == OP_NONE || archiveFilename == NULL) {
+		exitError("Usage: %s -f -t [archive-filename] [file1] [file2] ...", argv[0]);
 	}
 
-	FILE *archive = fopen(archive_filename, "rb");
+	FILE *archive = fopen(archiveFilename, "rb");
 
-	if (archive == NULL) {
-		exit_with_error("Cannot open archive file %s", archive_filename);
-	}
+    if (archive == NULL) {
+        exitError("Cannot open archive file %s", archiveFilename);
+    }
 
-	list_files(archive, argc, argv, filenames_start, found_files);
+    if (!isTarArchive(archive)) {
+        fclose(archive);
+        exitError("This does not look like a tar archive\nmytar: Exiting with failure status due to previous errors");
+    }
 
-	fclose(archive);
+    fseek(archive, 0, SEEK_SET); // Reset the file pointer to the beginning
+
+    if (op == OP_LIST) {
+        listFiles(archive, argc, argv, filenamesStart, foundFiles);
+    } else if (op == OP_EXTRACT) {
+        extractFiles(archive, argc, argv, filenamesStart, foundFiles, verbose);
+    }
+    fclose(archive);
 }
